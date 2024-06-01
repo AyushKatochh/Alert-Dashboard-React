887dcf7b1b9e9c771e5b152da25e4e762ec6e4d1
Ayush Katoch <katochayushhp@gmail.com>
2024-06-01 18:31:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,9 @@ const App = () => {
     fetchData();
   }, []);
 
-  const fetchData = async () => {
+  const fetchData = () => {
     try {
-      const response = await fetch('http://127.0.0.1:8080/data/transformedAlerts.json');
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      const jsonData = await response.json();
+      const jsonData = require('./data/transformedAlerts.json');
       setData(jsonData);
     } catch (error) {
       console.error('Error fetching data:', error);
